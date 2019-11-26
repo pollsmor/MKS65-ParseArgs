55a21c9a7965dd7e7b7a4961f9a728fa24020ca5
Kevin Li <kevinli4321@gmail.com>
2019-11-26 17:29:23 -0500

    Print helpful messages

--- main.c
+++ main.c
@@ -18,6 +18,8 @@ char ** parse_args(char * line) {
 
 int main() {
   char line[100] = "ls -a -l";
+  printf("Command being parsed: %s \n", line);
+  printf("Parsing...");
   char ** args = parse_args(line);
   execvp(args[0], args);
 
